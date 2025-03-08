a2a1ad2b39b5f76a38a79fd84d092dde8499f07a
chengetanai <mukanhairic@gmail.com>
2025-03-08 09:49:27 +0200

    feat: include predicitons router

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from .app import models
-from .app.database import engine
+from app import models
+from app.database import engine
+from app.routes import predictions
 
 app = FastAPI()
 
@@ -9,3 +10,5 @@ models.Base.metadata.create_all(bind=engine)
 @app.get('/')
 def index():
     return {'message': 'Welcome home'}
+
+app.include_router(predictions)
