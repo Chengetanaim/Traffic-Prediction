ade2cf61fd95701ecf7a011201802746d8e63601
chengetanai <mukanhairic@gmail.com>
2025-03-08 09:47:59 +0200

    feat: Prediction schema

---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class PredictionBase(BaseModel):
+    coded_day:int
+    zone:int
+    weather:int
+    temperature:int
+
+class PredictionCreate(PredictionBase):
+    pass
+
+class Prediction(PredictionBase):
+    traffic: float
